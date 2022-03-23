b91b71891608cd07d22e0e02af30f1d42e2d94a4
aruizr98 <77283313+aruizr98@users.noreply.github.com>
2022-03-23 21:26:51 +0100

    Rutas actualizadas.
    He actualizado las rutas. De momento, tenemos Inicio, foros, eventos, iniciar sesión, registrarse, mi cuenta y lista de mensajes.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CarrouselComponent } from './carrousel/carrousel.component';
+import {EventosComponent } from './eventos/eventos.component';
+import { IniciarSesionComponent } from './iniciar-sesion/iniciar-sesion.component';
+import {RegistrarseComponent } from './registrarse/registrarse.component';
+import { MiCuentaComponent } from './mi-cuenta/mi-cuenta.component';
+import { ListaMensajesComponent } from './lista-mensajes/lista-mensajes.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"", component:CarrouselComponent},
+  {path:"inicio", component:CarrouselComponent},
+  {path:"eventos", component:EventosComponent},
+  {path:"IniciarSesion", component:IniciarSesionComponent},
+  {path:"Registrarse", component:RegistrarseComponent},
+  {path:"Micuenta", component:MiCuentaComponent},
+  {path:"ListaMensajes", component:MiCuentaComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
