37adf2c1cd4985ea5284d1fa6d8cf7e10dd81159
aruizr98 <77283313+aruizr98@users.noreply.github.com>
2022-04-18 15:54:49 +0200

    Añadidos unos imports que  faltaban

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,38 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {HttpClientModule} from '@angular/common/http';
+import {FormsModule} from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavegacionComponent } from './navegacion/navegacion.component';
+import { CarrouselComponent } from './carrousel/carrousel.component';
+import { FooterComponent } from './footer/footer.component';
+import { EventosComponent } from './eventos/eventos.component';
+import { IniciarSesionComponent } from './iniciar-sesion/iniciar-sesion.component';
+import { RegistrarseComponent } from './registrarse/registrarse.component';
+import { MiCuentaComponent } from './mi-cuenta/mi-cuenta.component';
+import { ListaMensajesComponent } from './lista-mensajes/lista-mensajes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavegacionComponent,
+    CarrouselComponent,
+    FooterComponent,
+    EventosComponent,
+    IniciarSesionComponent,
+    RegistrarseComponent,
+    MiCuentaComponent,
+    ListaMensajesComponent,
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
