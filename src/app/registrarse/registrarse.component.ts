baafc1457cad3be765197783f16f7220b8bedcf2
aruizr98 <77283313+aruizr98@users.noreply.github.com>
2022-04-18 15:52:42 +0200

    Modificado para acceder a la base de datos

--- src/app/registrarse/registrarse.component.ts
+++ src/app/registrarse/registrarse.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
-
+import {Usuario} from "../models/usuario";
+import {UsuarioService} from "../services/usuario.service";
+import { Global } from '../services/Global';
 @Component({
   selector: 'app-registrarse',
   templateUrl: './registrarse.component.html',
-  styleUrls: ['./registrarse.component.css']
+  styleUrls: ['./registrarse.component.css'],
+  providers:[UsuarioService]
 })
 export class RegistrarseComponent implements OnInit {
-
-  constructor() { }
+  public usuario:Usuario;
+  public url:string;
+  constructor(
+    private _usuarioService:UsuarioService
+  ) {
+    this.url=Global.url; 
+    this.usuario=new Usuario("", "", "", "", "", "", false, "");
+  }
 
   ngOnInit(): void {
   }
-
+  onSubmit(form){
+    console.log(this.usuario);
+    this._usuarioService.guardarUsuario(this.usuario).subscribe(
+      response => {
+        console.log(response);
+        
+      },
+      error =>{
+        console.log(<any>error);
+      }
+    )
+  }
 }
