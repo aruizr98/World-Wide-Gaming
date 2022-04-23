e4974db02b720fcabd90c8040cde11f613ae888b
aruizr98 <77283313+aruizr98@users.noreply.github.com>
2022-04-23 16:33:36 +0200

    Actualizado para cambiar botones al iniciar sesión
    Ahora al entrar con una sesión iniciada, desaparecen los botones de inicio de sesión y registro y aparecen el de mi cuenta y el de lista de mensajes

--- src/app/navegacion/navegacion.component.ts
+++ src/app/navegacion/navegacion.component.ts
@@ -6,10 +6,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navegacion.component.css']
 })
 export class NavegacionComponent implements OnInit {
-
+  public sessionStorage:boolean;
+  public localStorage:boolean;
   constructor() { }
 
   ngOnInit(): void {
+    if(localStorage.getItem("nombreUsuario") !=undefined){
+      this.localStorage=true;
+      console.log(this.localStorage);
+    }else{
+      this.localStorage=false;
+      console.log(this.localStorage);
+      if(sessionStorage.getItem("nombreUsuario") !=undefined){
+        this.sessionStorage=true;
+        console.log("SessionStorage");
+      }
+    }
   }
 
 }
