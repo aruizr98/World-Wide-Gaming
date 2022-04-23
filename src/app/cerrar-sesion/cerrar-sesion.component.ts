f04ebdbab2b6dc30370157ccf843074211fb2521
aruizr98 <77283313+aruizr98@users.noreply.github.com>
2022-04-23 16:35:18 +0200

    Componte para cerrar sesión.
    Al entrar, se  borra el sessionStorage y el localStorage llamado nombreUsuario

---
+++ src/app/cerrar-sesion/cerrar-sesion.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cerrar-sesion',
+  templateUrl: './cerrar-sesion.component.html',
+  styleUrls: ['./cerrar-sesion.component.css']
+})
+export class CerrarSesionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+    sessionStorage.removeItem('nombreUsuario');
+    localStorage.removeItem('nombreUsuario');
+    location.href="/Inicio";
+  }
+
+}
