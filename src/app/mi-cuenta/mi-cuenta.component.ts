d8e3fcb45ff1d93910011e89c8f712050b6af1a9
aruizr98 <77283313+aruizr98@users.noreply.github.com>
2022-04-30 09:31:31 +0200

    Actualizado para mostrar o no el botón Administrar
    Ahora, si el usuario es administrador se muestra el botón de administrar, pero si no lo es no se muestra.

--- src/app/mi-cuenta/mi-cuenta.component.ts
+++ src/app/mi-cuenta/mi-cuenta.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Usuario } from "../models/usuario";
+import { UsuarioService } from "../services/usuario.service";
 
 @Component({
   selector: 'app-mi-cuenta',
@@ -7,13 +9,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MiCuentaComponent implements OnInit {
   public nombreUsuario:string;
-  constructor() { }
+  public administrador:boolean;
+  constructor(private _usuarioService: UsuarioService) { }
 
   ngOnInit(): void {
     if(localStorage.getItem("nombreUsuario") !=undefined){
-      this.nombreUsuario = localStorage.getItem("nombreUsuario")
+      this.nombreUsuario = localStorage.getItem("nombreUsuario");
+      this._usuarioService.listarUsuarios().subscribe(
+        response => {
+          for (let index = 0; index < response["usuarios"].length; index++) {
+           if(response["usuarios"][index].NombreUsuario == this.nombreUsuario){//Obtener el usuario completo a partir del nombre de usuario para saber si es admin o no
+            this.administrador=response["usuarios"][index].Administrador;
+           }
+          }
+  
+        },
+        error => {
+          console.log(<any>error);
+        }
+      )
+      
     }else{
-      this.nombreUsuario=sessionStorage.getItem("nombreUsuario")
+      this.nombreUsuario=sessionStorage.getItem("nombreUsuario");
+      this._usuarioService.listarUsuarios().subscribe(
+        response => {
+          for (let index = 0; index < response["usuarios"].length; index++) {
+           if(response["usuarios"][index].NombreUsuario == this.nombreUsuario){//Obtener el usuario completo a partir del nombre de usuario para saber si es admin o no
+            this.administrador=response["usuarios"][index].Administrador;
+           }
+          }
+  
+        },
+        error => {
+          console.log(<any>error);
+        }
+      )
     }
   }
 
