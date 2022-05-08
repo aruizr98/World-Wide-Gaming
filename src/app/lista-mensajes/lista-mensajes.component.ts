6fcdb7153034e749d9851b4bff88c70fa3ff5e48
aruizr98 <77283313+aruizr98@users.noreply.github.com>
2022-05-08 16:18:35 +0200

    Se muestran los mensajes por pantalla
    Aunque falta que se puedan responder

--- src/app/lista-mensajes/lista-mensajes.component.ts
+++ src/app/lista-mensajes/lista-mensajes.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Mensaje } from "../models/Mensaje";
+import { MensajeService } from "../services/mensaje.service"
+import { UsuarioService } from "../services/usuario.service";
+
 
 @Component({
   selector: 'app-lista-mensajes',
@@ -6,10 +10,67 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./lista-mensajes.component.css']
 })
 export class ListaMensajesComponent implements OnInit {
-
-  constructor() { }
+  public mensajes:boolean;
+  constructor(private _usuarioService: UsuarioService, private _mensajeService: MensajeService) { }
 
   ngOnInit(): void {
+    if(sessionStorage.getItem("mensajes") == "true"){
+      this.mensajes=true;
+    }else{
+      this.mensajes=false;
+    }
+
+    this._usuarioService.listarUsuarios().subscribe(
+      response => {
+        console.log(response["usuarios"])
+        for (let index = 0; index < response["usuarios"].length; index++) {
+          if (response["usuarios"][index].NombreUsuario == sessionStorage.getItem("nombreUsuario")) {
+            sessionStorage.setItem("idUsuario", response["usuarios"][index]._id);
+          }
+        }
+
+      },
+      error => {
+        console.log(<any>error);
+      }
+    )
+    this._mensajeService.listarMensajes().subscribe(
+      response => {
+        console.log(response["mensajes"])
+        for (let index = 0; index < response["mensajes"].length; index++) {
+          if(response["mensajes"][index].Receptor == sessionStorage.getItem("idUsuario")){
+            this._usuarioService.listarUsuarios().subscribe(
+              response2 => {
+                for (let j = 0; j < response2["usuarios"].length; j++) {
+                  if(response2["usuarios"][j]._id == response["mensajes"][index].Emisor){
+                    let nombreEmisor=response2["usuarios"][j].NombreUsuario;
+                    sessionStorage.setItem("mensajes", "true");
+                    let mensajes=document.getElementById("mensajes");
+                    let div=document.createElement("div");
+                    div.setAttribute("class", "border border-black w-25 mx-auto")
+                    div.innerHTML=`
+                      <span>De `+nombreEmisor+`</span>
+                      <p>`+response["mensajes"][j].Mensaje+`</p>
+                    `
+                    mensajes.append(div);
+                  }
+                  
+                }
+              },
+              error => {
+                console.log(<any>error);
+              }
+            )
+           
+            // console.log(this._usuarioService.obtenerNombreUsuario(response["mensajes"][index]._id));
+          }
+        }
+
+      },
+      error => {
+        console.log(<any>error);
+      }
+    )
   }
 
 }
