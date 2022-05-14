bc27dfb72d0d0ecf5c157825317ef26144449680
aruizr98 <77283313+aruizr98@users.noreply.github.com>
2022-05-14 22:20:30 +0200

    Componente para enviar mensajes

---
+++ src/app/enviar-mensaje/enviar-mensaje.component.ts
@@ -0,0 +1,50 @@
+import { Component, OnInit } from '@angular/core';
+import {Mensaje} from '../models/Mensaje';
+import { MensajeService } from '../services/mensaje.service';
+import{UsuarioService} from '../services/usuario.service';
+import { Global } from '../services/global';
+
+@Component({
+  selector: 'app-enviar-mensaje',
+  templateUrl: './enviar-mensaje.component.html',
+  styleUrls: ['./enviar-mensaje.component.css'],
+  providers:[MensajeService]
+})
+export class EnviarMensajeComponent implements OnInit {
+  public mensaje:Mensaje;
+  public url:string;
+  constructor(private _usuarioService:UsuarioService, private _mensajeService:MensajeService) {
+    this.url=Global.url; 
+    if(sessionStorage.getItem("idUsuario") != undefined){
+    this.mensaje=new Mensaje("", "", "", sessionStorage.getItem("idUsuario"));
+    }else{
+      this.mensaje=new Mensaje("", "", "", localStorage.getItem("idUsuario")); 
+    }
+   }
+
+  ngOnInit(): void {
+    this._usuarioService.listarUsuarios().subscribe(
+      response =>{
+        console.log(response["usuarios"]);
+        for (let index = 0; index < response["usuarios"].length; index++) {
+          document.getElementById("receptores").innerHTML+=`
+            <option value='`+response["usuarios"][index]._id+`'>`+response["usuarios"][index].NombreUsuario+`</option>
+          `
+        };
+      },
+      error =>{
+        console.log(<any>error);
+      }
+    )
+  }
+  onSubmit(){
+   this._mensajeService.crearMensjae(this.mensaje).subscribe(
+     response=>{
+      console.log(response);
+     },
+     error=>{
+       console.log(<any>error);
+     }
+   )
+  }
+}
