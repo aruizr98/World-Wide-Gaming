ec224f46571379e658f34b3b05e0f3397c217311
aruizr98 <77283313+aruizr98@users.noreply.github.com>
2022-05-16 08:04:10 +0200

    Añadido método crearMensaje

--- src/app/services/mensaje.service.ts
+++ src/app/services/mensaje.service.ts
@@ -19,4 +19,10 @@ export class MensajeService {
 
     return this._http.get(this.url+"mensajes", {headers:headers});
    }
+   crearMensjae(mensaje:Mensaje):Observable<any>{
+    let params=JSON.stringify(mensaje);
+    let headers=new HttpHeaders().set("content-type", "application/json");
+
+    return this._http.post(this.url+"crearMensaje", params, {headers:headers});
+   }
 }
