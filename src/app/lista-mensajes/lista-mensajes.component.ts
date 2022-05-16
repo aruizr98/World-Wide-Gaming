b6947791b147431a944b8ab74dd3270e41d8023a
aruizr98 <77283313+aruizr98@users.noreply.github.com>
2022-05-16 16:21:36 +0200

    Implementada vista de chat.
    Ahora los mensajes están agrupados por emisor y se muestran a la izquierda los mensajes del emisor y a la derecha los del usuario conectado (como en un chat).

--- src/app/lista-mensajes/lista-mensajes.component.ts
+++ src/app/lista-mensajes/lista-mensajes.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Mensaje } from "../models/Mensaje";
 import { MensajeService } from "../services/mensaje.service"
 import { UsuarioService } from "../services/usuario.service";
-import {AppModule} from "../app.module"
+import { AppModule } from "../app.module"
 
 
 @Component({
@@ -11,51 +11,100 @@ import {AppModule} from "../app.module"
   styleUrls: ['./lista-mensajes.component.css']
 })
 export class ListaMensajesComponent implements OnInit {
-  public mensajes:boolean;
+  public mensajes: boolean;
   constructor(private _usuarioService: UsuarioService, private _mensajeService: MensajeService) { }
 
   ngOnInit(): void {
-    this._mensajeService.listarMensajes().subscribe(
-      response => {
-        console.log(response["mensajes"])
-        for (let index = 0; index < response["mensajes"].length; index++) {
-          if(response["mensajes"][index].Receptor == sessionStorage.getItem("idUsuario") ||response["mensajes"][index].Receptor == localStorage.getItem("idUsuario") ){
-            this._usuarioService.listarUsuarios().subscribe(
-              response2 => {
-                for (let j = 0; j < response2["usuarios"].length; j++) {
-                  if(response2["usuarios"][j]._id == response["mensajes"][index].Emisor){
-                    let nombreEmisor=response2["usuarios"][j].NombreUsuario;
-                    localStorage.setItem("mensajes", "true");
-                    let mensajes=document.getElementById("mensajes");
-                    let div=document.createElement("div");
-                    div.setAttribute("class", "border border-black w-25 mx-auto");
-                    div.innerHTML=`
-                      <span>De `+nombreEmisor+`</span>
-                      <p>`+response["mensajes"][index].Mensaje+`</p>
-                      <button class='btn btn-primary my-1' onclick="sessionStorage.setItem('ReceptorMensaje', '`+response2["usuarios"][j].NombreUsuario+`'); sessionStorage.setItem('idReceptorMensaje', '`+response2["usuarios"][j]._id+`')">Responder</button>
-                    `
-                    mensajes.append(div);
-                  }
-                  
-                }
-              },
-              error => {
-                console.log(<any>error);
-              }
-            )
-          }
-        }
+    // this._mensajeService.listarMensajes().subscribe(
+    //   response => {
+    //     console.log(response["mensajes"])
+    //     for (let index = 0; index < response["mensajes"].length; index++) {
+    //       if(response["mensajes"][index].Receptor == sessionStorage.getItem("idUsuario") ||response["mensajes"][index].Receptor == localStorage.getItem("idUsuario") ){
+    //         this._usuarioService.listarUsuarios().subscribe(
+    //           response2 => {
+    //             for (let j = 0; j < response2["usuarios"].length; j++) {
+    //               if(response2["usuarios"][j]._id == response["mensajes"][index].Emisor){
+    //                 let nombreEmisor=response2["usuarios"][j].NombreUsuario;
+    //                 localStorage.setItem("mensajes", "true");
+    //                 let mensajes=document.getElementById("mensajes");
+    //                 let div=document.createElement("div");
+    //                 div.setAttribute("class", "border border-black w-25 mx-auto");
+    //                 div.innerHTML=`
+    //                   <span>De `+nombreEmisor+`</span>
+    //                   <p>`+response["mensajes"][index].Mensaje+`</p>
+    //                   <button class='btn btn-primary my-1' onclick="sessionStorage.setItem('ReceptorMensaje', '`+response2["usuarios"][j].NombreUsuario+`'); sessionStorage.setItem('idReceptorMensaje', '`+response2["usuarios"][j]._id+`')">Responder</button>
+    //                 `
+    //                 mensajes.append(div);
+    //               }
 
+    //             }
+    //           },
+    //           error => {
+    //             console.log(<any>error);
+    //           }
+    //         )
+    //       }
+    //     }
+
+    //   },
+    //   error => {
+    //     console.log(<any>error);
+    //   }
+    // )
+    this._usuarioService.listarUsuarios().subscribe(
+
+      response => {
+        for (let index = 0; index < response["usuarios"].length; index++) {
+          this._mensajeService.listarMensajes().subscribe(
+            response2 => {
+              let mensajes = false;
+              let arrayMensajes = new Array();
+              var caja = document.createElement("div");
+              let contador=0;
+              caja.setAttribute("class", "border border-black w-25 mx-auto my-3 px-2");
+              var mensajesCaja = document.getElementById("mensajes");
+              for (let j = 0; j < response2["mensajes"].length; j++) {
+                if (response["usuarios"][index]._id == response2["mensajes"][j].Emisor && (response2["mensajes"][j].Receptor == sessionStorage.getItem("idUsuario") || response2["mensajes"][j].Receptor == localStorage.getItem("idUsuario"))) {
+                  if(contador==0){
+                    caja.innerHTML += "<div class='text-center'><span><b>De " + response["usuarios"][index].NombreUsuario + "</b></span></div>";
+                    contador++;
+                  }
+                  localStorage.setItem("mensajes", "true");
+                  mensajes = true;
+                  // arrayMensajes.push(response2["mensajes"][j]);
+                  caja.innerHTML += "<p class='m-0 text-start'>" + response2["mensajes"][j].Mensaje + "</p>";
+                }else if((response2["mensajes"][j].Emisor == sessionStorage.getItem("idUsuario") || response2["mensajes"][j].Emisor == localStorage.getItem("idUsuario")) && response["usuarios"][index]._id == response2["mensajes"][j].Receptor  ){
+                  if(contador==0){
+                    caja.innerHTML += "<div class='text-center'><span><b>De " + response["usuarios"][index].NombreUsuario + "</b></span></div>";
+                    contador++;
+                  }
+                  // arrayMensajesEmisor.push(response2["mensajes"][j]);
+                  caja.innerHTML += "<p class='m-0 text-end'>" + response2["mensajes"][j].Mensaje + "</p>";
+                }
+
+              }
+              if (mensajes) {
+                
+                mensajesCaja.append(caja);
+              }
+             
+            },
+            error => {
+              console.log(<any>error);
+            }
+          )
+
+        }
       },
       error => {
         console.log(<any>error);
       }
     )
   }
-  comprobarMensajes():boolean{
-    if(localStorage.getItem("mensajes") == "true"){
+  comprobarMensajes(): boolean {
+    if (localStorage.getItem("mensajes") == "true") {
       return true;
-    }else{
+    } else {
       return false;
     }
   }
