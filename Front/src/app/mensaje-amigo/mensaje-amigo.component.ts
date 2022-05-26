ca9c640986b0e0e2af61dcb4788accd5263ceee3
aruizr98 <77283313+aruizr98@users.noreply.github.com>
2022-05-26 16:51:48 +0200

    Mejorado para funcionar de las 2 formas
    Tanto si el usuario pulsa el botón de escribir mensaje dentro de un chat (en este caso el receptor sale marcado y no se puede cambiar) como si pulsa el botón que está fuera de los chats (en este caso aparece una barra de búsqueda para que el usuario pueda escribir el receptor que quiera)

--- Front/src/app/mensaje-amigo/mensaje-amigo.component.ts
+++ Front/src/app/mensaje-amigo/mensaje-amigo.component.ts
@@ -10,6 +10,7 @@ import { UsuarioService } from '../services/usuario.service';
 })
 export class MensajeAmigoComponent implements OnInit {
   public amigo: string = "";
+  public receptorMarcado:boolean;
   public status: boolean;
   public err: Number;
   public mensaje: Mensaje = new Mensaje("", "", "", "");
@@ -17,11 +18,48 @@ export class MensajeAmigoComponent implements OnInit {
   constructor(private _usuarioService: UsuarioService, private _mensajeService: MensajeService) { }
 
   ngOnInit(): void {
-    this.err = 0;
+    if(sessionStorage.getItem("amigo")){
+      this.err = 0;
     this.status = false;
     this.amigo = sessionStorage.getItem("amigo");
+    this.receptorMarcado=true;
+    sessionStorage.removeItem("amigo");
+    }else{
+      this.receptorMarcado=false;
+    }
+    
   }
+  ngOnInput() {
+    this._usuarioService.listarUsuarios().subscribe(
+      response => {
+        document.getElementById("resultado").innerHTML = "";
+        for (let index = 0; index < response["usuarios"].length; index++) {
+          if (response["usuarios"][index]._id == sessionStorage.getItem("idUsuario") || response["usuarios"][index]._id == localStorage.getItem("idUsuario")) {
+            for (let j = 0; j < response["usuarios"][index].Favoritos.length; j++) {
+              this._usuarioService.listarUsuario(response["usuarios"][index].Favoritos[j]).subscribe(
+                response2 => {
 
+                  if (response2["usuario"].NombreUsuario.indexOf(this.mensaje.Receptor) != -1) {
+                    console.log(response2["usuario"].NombreUsuario);
+                    document.getElementById("resultado").innerHTML += `
+                  <div role='button' onclick="document.getElementById('busqueda').value='`+ response2["usuario"].NombreUsuario + `'; sessionStorage.setItem('receptor', '` + response2["usuario"]._id + `'); sessionStorage.setItem('nombreReceptor', '` + response2["usuario"].NombreUsuario + `');"><h5>` + response2["usuario"].NombreUsuario + `</h5></div>
+                `
+                  }
+                },
+                error => {
+                  console.log(<any>error);
+                }
+              )
+
+            }
+          }
+        }
+      },
+      error => {
+        console.log(<any>error);
+      }
+    )
+  }
   enviarMensaje(): void {
     var val = document.forms["formulario"]["mensaje"].value;
     if (val == "") {
@@ -29,6 +67,9 @@ export class MensajeAmigoComponent implements OnInit {
     } else {
       this.mensaje.Mensaje = val;
       this.mensaje.Emisor = sessionStorage.getItem("idUsuario");
+      if(!sessionStorage.getItem("amigo")){
+        this.mensaje.Receptor=sessionStorage.getItem("receptor");
+      }
       this._usuarioService.listarUsuarios().subscribe(
         response => {
           for (let index = 0; index < response["usuarios"].length; index++) {
