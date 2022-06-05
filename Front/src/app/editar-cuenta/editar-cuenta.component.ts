0d999921f7fc6d18f66a1f143ab69655b3d2f327
aruizr98 <77283313+aruizr98@users.noreply.github.com>
2022-06-05 22:18:48 +0200

    Añadida foto de perfil

--- Front/src/app/editar-cuenta/editar-cuenta.component.ts
+++ Front/src/app/editar-cuenta/editar-cuenta.component.ts
@@ -3,6 +3,7 @@ import { UsuarioService } from "../services/usuario.service";
 import { Global } from '../services/global';
 import { Usuario } from '../models/usuario';
 import { Router } from '@angular/router';
+import { UploadService } from '../services/upload.service';
 
 @Component({
   selector: 'app-editar-cuenta',
@@ -13,8 +14,9 @@ export class EditarCuentaComponent implements OnInit {
   public nombreUsuario: string;
   public url: string;
   public usuario: Usuario;
+  public filesToUpload: Array<File>
 
-  constructor(private _usuarioService: UsuarioService, private router: Router) { this.url = Global.url;this.usuario = new Usuario("", "", "", "", "", "", false, "", [], [], "", "", "", "", "", "", "", ""); }
+  constructor(private _usuarioService: UsuarioService, private router: Router,  private _uploadService: UploadService) { this.url = Global.url;this.usuario = new Usuario("", "", "", "", "", "", false, "", [], [], "", "", "", "", "", "", "", ""); }
 
   ngOnInit(): void {
     if (localStorage.getItem("nombreUsuario") != undefined) {
@@ -43,15 +45,29 @@ export class EditarCuentaComponent implements OnInit {
     }else{
       localStorage.setItem("nombreUsuario", this.usuario.NombreUsuario);
     }
-   
+    
     this._usuarioService.editarUsuario(this.usuario._id, this.usuario).subscribe(
       response => {
         console.log(response);
         document.getElementById("correcto").setAttribute("class", "alert alert-success d-block text-center");
         document.getElementById("correcto").innerText="Se ha modificado el usuario correctamente";
+        console.log(this.usuario.FotoPerfil)
+        if (this.usuario.FotoPerfil != "") {
+          this._uploadService.makeFileRequest(Global.url + "subirImagen/" + response["usuario"]._id, [], this.filesToUpload, "image").then((result: any) => {
+          })
+        }
       })
-    this.router.routeReuseStrategy.shouldReuseRoute = () => false;
-    this.router.onSameUrlNavigation = "reload";
-    this.router.navigate(["/EditarCuenta"]);
+  }
+  fileChangeEvent(fileInput: any) {
+    this.filesToUpload = <Array<File>>fileInput.target.files;
+  }
+  elminarFotoPerfil(){
+    let respuesta = confirm("Seguro que quieres eliminar la foto de perfil actual?");
+    if(respuesta){
+      this.usuario.FotoPerfil="";
+      alert("Foto de perfil eliminada correctamente");
+    }else{
+      alert("No se ha eliminado la foto de perfil");
+    }
   }
 }
