cf27379d9d0f9d413022e207d28bf48ed5214a49
aruizr98 <77283313+aruizr98@users.noreply.github.com>
2022-06-12 15:02:49 +0200

    Nuevos colores añadidos

--- Front/src/app/carrousel/carrousel.component.ts
+++ Front/src/app/carrousel/carrousel.component.ts
@@ -10,6 +10,8 @@ export class CarrouselComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    document.getElementById("cajaLogo").setAttribute("class", "animarLogo");
+    document.getElementById("listaMenu").setAttribute("class", "nav col-8 justify-content-end animarMenu");
   }
 
 }
